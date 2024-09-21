56ea9b12a275e6d744c7c75a2df0fd05cced6778
sunny <ethsok@protonmail.com>
2024-09-21 19:09:59 -0400

    simple calculator dine!!!

--- simple Calc/script.js
+++ simple Calc/script.js
@@ -5,38 +5,56 @@ const answer = document.getElementById('answer')
 
 
 function add(a,b){
-    let first = num1.value
-    let second = num2.value
+    let first = parseFloat(num1.value)
+    let second = parseFloat(num2.value)
+    let result = first + second
+    answer.innerText = result
 
 }
 
 function multiply(a,b){
-    let first = num1.value
-    let second = num2.value
+    let first = parseFloat(num1.value)
+    let second = parseFloat(num2.value)
+    let result = first * second
+    answer.innerText = result
 
 }
 
 function divide(a,b){
-    let first = num1.value
-    let second = num2.value
+    let first = parseFloat(num1.value)
+    let second = parseFloat(num2.value)
+    let result = first / second
+    answer.innerText = result
 
 }
 
 function subtract(a,b){
-    let first = num1.value
-    let second = num2.value
+    let first = parseFloat(num1.value)
+    let second = parseFloat(num2.value)
+    let result = first - second
+    answer.innerText = result
 
 }
 
 
 function power(a,b){
-    let first = num1.value
-    let second = num2.value
+    let first = parseFloat(num1.value)
+    let second = parseFloat(num2.value)
+    let result = 1
+
+    for(let i = 0; i < second ;i++){
+        result *= first
+        
+    }
+    answer.innerText = result
+
+    
 
 }
 
 function square(a,b){
     let first = num1.value
     let second = num2.value
+
 
 }
