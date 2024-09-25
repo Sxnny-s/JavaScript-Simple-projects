2b82fa2e69b2b3ab2bfaa2cc177cf4c836012bae
sunny <ethsok@protonmail.com>
2024-09-25 09:50:20 -0400

    Displayed the slot machine in the terminal

--- Slot Machine/text.js
+++ Slot Machine/text.js
@@ -9,7 +9,15 @@ const slotDisplay = () => {
         ['🔔', '💰', '🍒'],
         ['🍋', '🍊', '🔔'],
     ];
-
+    
+    for(o = 0; o < 3; o++){
+        let rowDisplay = '';
+        for(i = 0; i < 3; i++){
+            rowDisplay  += reels[o][i] + " "
+        }
+        
+        console.log(rowDisplay)
+    }
    
 
     
