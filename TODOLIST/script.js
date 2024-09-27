99115af1902cc4532bda0a75e943ce267becdcdc
sunny <ethsok@protonmail.com>
2024-09-27 13:25:54 -0400

    finnished todolist

--- TODOLIST/script.js
+++ TODOLIST/script.js
@@ -1,1 +1,29 @@
-parse
+let input = document.getElementsByTagName('input')[0];
+let list = document.getElementsByTagName('ul')[0]
+
+
+// add items 
+function addItem(){
+    let inputValue = input.value
+
+    if (inputValue == ''){
+        list.innerHTML += ``
+    }else {
+        list.innerHTML += `<li onclick="completed(event)" >${inputValue}</li>`
+    }
+}
+
+// clears list
+function clearList(){
+    list.innerHTML = '';
+}
+
+
+// complete task
+
+function completed(event){
+
+        event.target.classList.toggle ('completed')
+}
+
+
